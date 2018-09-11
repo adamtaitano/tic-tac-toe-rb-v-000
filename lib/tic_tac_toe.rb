076edd9f1a3b2cd4f2c49525d735bb1162b7f518
Adam Taitano <adam.taitano@gmail.com>
2018-09-11 19:37:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,5 +60,10 @@ def current_player(board)
 end
 
 def won?(board)
-  
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
 end
+
